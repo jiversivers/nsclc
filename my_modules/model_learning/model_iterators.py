fixed datatype issue caused by normalizing, made some other hard-code dtypes to be safe, updated model paths for clarity

my_modules/model_learning/model_iterators.py
@@ -49,7 +49,7 @@ def single_model_iterator(models, datasets, epochs, batch_size, criterion, optim
             #################
             model = net(data_shape)
             model = model.to('cuda') if torch.cuda.is_available() else model
-            model_path = f'./{key}_{model.name}.pth'
+            model_path = f'./{key}_{model.name}_{epochs}.pth'
 
             optimizer = optim_fun(model.parameters(), **kwargs)
 
@@ -61,7 +61,7 @@ def single_model_iterator(models, datasets, epochs, batch_size, criterion, optim
             eval_loss = []
 
             for epoch in range(epochs):
-                print(f'>>>{model.name} Epoch {epoch+1}...')
+                print(f'>>>{model.name} Epoch {epoch+1}/{epochs}...')
                 epoch_loss = train_epoch(model, train_loader, criterion, optimizer)
                 tran_loss.append(epoch_loss)
 
@@ -90,7 +90,7 @@ def single_model_iterator(models, datasets, epochs, batch_size, criterion, optim
             accu = correct / len(test_set)
 
             results[key][model.name] = accu
-            show_results(model.name, set, accu, tran_loss, eval_loss, eval_accu)
+            show_results(model.name, data.name, accu, tran_loss, eval_loss, eval_accu)
 
     return results
 
@@ -129,7 +129,6 @@ def fold_model_iterator(models, datasets, folds, epochs, batch_size, criterion, 
                                                           drop_last=True,
                                                           num_workers=workers[2],
                                                           prefetch_factor=prefetch_factor)
-
                 #######################
                 # Prepare fresh model #
                 #######################
@@ -162,7 +161,7 @@ def fold_model_iterator(models, datasets, folds, epochs, batch_size, criterion, 
             accu = sum(k_results[key][model.name]) / folds
 
             results[key][model.name] = accu
-            show_results(model.name, set, accu, tran_loss, eval_loss, eval_accu)
+            show_results(model.name, data.name, accu, tran_loss, eval_loss, eval_accu)
 
     return results, k_results
 

my_modules/nsclc/image_loaders.py
@@ -19,7 +19,7 @@ def load_asc(load_fns_and_image_paths):
 
 # Function to simplify getting mean lifetime from raw parameters
 def load_weighted_average(load_fns_and_img_paths):
-    x = torch.Tensor([])
+    x = torch.empty(0, dtype=torch.float32)
     for fp in load_fns_and_img_paths[1]:
         x = torch.cat((x, torch.unsqueeze(fp[0](fp), dim=0)), dim=0)
     x = (x[0] * x[1] + x[2] * x[3]) / (x[0] + x[2])

my_modules/nsclc/nsclc_dataset.py
@@ -139,7 +139,7 @@ class NSCLCDataset:
             case _:
                 raise Exception('No data label selected. Update label attribute of dataset and try again.')
 
-        # Apply distirbution transform, if called
+        # Apply distribution transform, if called
         if self.dist_transformed:
             X_dist = torch.empty((self.stack_height,) + (self.nbins,), dtype=torch.float32)
             for ch, mode in enumerate(X):
@@ -152,7 +152,7 @@ class NSCLCDataset:
         self.nbins = nbins
         if not self.normalized:
             print(
-                'Normalization is automatically applied for the distribtution transform.\n     '
+                'Normalization is automatically applied for the distribution transform.\n     '
                 'This can be manually overwritten by setting the NORMALIZED attribute to False')
             self.normalize_channels_to_max()
         self.dist_transformed = True
@@ -166,7 +166,6 @@ class NSCLCDataset:
                 ax[ii].legend()
                 ax[ii].tick_params(top=False, bottom=False, left=False, right=False, labelleft=False, labelbottom=False)
                 ax[ii].set_title(f'Label: {self[index][1]}', fontsize=10)
-            plt.show()
         else:
             transform = transforms.ToPILImage()
             _, ax = plt.subplots(5, len(self.mode), figsize=(10, 10))
@@ -185,7 +184,7 @@ class NSCLCDataset:
                     ax[ii].tick_params(top=False, bottom=False, left=False, right=False, labelleft=False,
                                        labelbottom=False)
                     ax[ii].set_title(f'Response: {lab}. \n Mode: {self.mode[jj]}', fontsize=10)
-            plt.show()
+        plt.show()
 
     def augment(self):
         self.augmented = True
@@ -205,7 +204,7 @@ class NSCLCDataset:
         self.augmented = False
 
         # Preallocate an array. Each row is an individual image, each column is mode
-        maxes = np.zeros((len(self), self.stack_height))
+        maxes = np.zeros((len(self), self.stack_height), dtype=np.float32)
         for ii, (stack, _) in enumerate(self):
             maxes[ii] = np.nanmax(stack, axis=(1, 2))
         self.scalars = np.max(maxes, axis=0)