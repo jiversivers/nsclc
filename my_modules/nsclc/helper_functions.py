d385d89d57814dd77e9758f1c7f0f4087b96b2b2
jiversivers <jdivers@uark.edu>
2024-05-23 13:52:47 -0500

    made dtypes explicit and made loader function empty array init consistent for clarity

--- my_modules/nsclc/helper_functions.py
+++ my_modules/nsclc/helper_functions.py
@@ -6,19 +6,19 @@ import numpy as np
 
 # Function to load TIFFs
 def load_tiff(load_fns_and_img_paths):
-    X = transforms.ToTensor()(Image.open(load_fns_and_img_paths[1])).float()
+    X = transforms.ToTensor()(Image.open(load_fns_and_img_paths[1])).to(torch.float32)
     return X
 
 
 # Function to load ASCs
 def load_asc(load_fns_and_img_paths):
-    x = torch.from_numpy(np.genfromtxt(load_fns_and_img_paths[1])).float()
+    x = torch.from_numpy(np.genfromtxt(load_fns_and_img_paths[1])).to(torch.float32)
     return x
 
 
 # Function to simplify getting mean lifetime from raw parameters
 def load_weighted_average(load_fns_and_img_paths):
-    x = torch.empty(0, dtype=torch.float32)
+    x = torch.tensor([], dtype=torch.float32)
     for fp in load_fns_and_img_paths[1]:
         x = torch.cat((x, torch.unsqueeze(fp[0](fp), dim=0)), dim=0)
     x = (x[0] * x[1] + x[2] * x[3]) / (x[0] + x[2])
@@ -28,7 +28,7 @@ def load_weighted_average(load_fns_and_img_paths):
 
 # Function to simplify getting mean lifetime from raw parameters
 def load_bound_fraction(load_fns_and_img_paths):
-    x = torch.Tensor([])
+    x = torch.tensor([], dtype=torch.float32)
     for fp in load_fns_and_img_paths[1]:
         x = torch.cat((x, torch.unsqueeze(fp[0](fp), dim=0)), dim=0)
     X = x[1] / (x[0] + x[1])
