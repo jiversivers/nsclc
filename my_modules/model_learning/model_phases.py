75ed1b12d87ad5e9a8b8e68a5bc7f44b384aac01
jiversivers <jdivers@uark.edu>
2024-05-29 12:03:06 -0500

    revamped masked_loss to return a function that can be used like a torch.nn loss. Added masked_loss_fn arg to training and eval

--- my_modules/model_learning/model_phases.py
+++ my_modules/model_learning/model_phases.py
@@ -2,15 +2,17 @@ import torch
 from torch import nn
 
 
-def masked_loss(loss_fn, predictions, targets, image):
-    mask = (~torch.isnan(image)).float()
-    loss = loss_fn(predictions, targets)
-    masked = mask * loss
-    masked = torch.sum(masked) / torch.sum(mask)  # Loss averaged over pixels that were numeric
-    return masked
+def masked_loss(loss_fn):
+    def masked_loss_fn(outputs, targets, mask):
+        loss = loss_fn(outputs.expand(mask.shape), targets.expand(mask.shape))
+        loss = loss * mask.float()
+        loss = torch.sum(loss) / torch.sum(mask.float())
+        return loss
+
+    return masked_loss_fn
 
 
-def train_epoch(model, loader, loss_fun, optimizer):
+def train_epoch(model, loader, loss_fn, optimizer, masked_loss_fn=False):
     model.train()
     total_loss = 0
     if torch.cuda.is_available() and not next(model.parameters()).is_cuda:
@@ -21,7 +23,7 @@ def train_epoch(model, loader, loss_fun, optimizer):
             target = target.cuda() if target.device.type != 'cuda' else target
         optimizer.zero_grad()
         out = model(x).squeeze(1)
-        loss = loss_fun(out.float(), target.float())
+        loss = loss_fn(out, target.float(), (~torch.isnan(x))) if masked_loss_fn else loss_fn(out, target.float())
         total_loss += loss.item()
         loss.backward()
         optimizer.step()
@@ -29,7 +31,7 @@ def train_epoch(model, loader, loss_fun, optimizer):
     return epoch_loss
 
 
-def valid_epoch(model, loader, loss_fun):
+def valid_epoch(model, loader, loss_fn, masked_loss_fn=False):
     model.eval()
     if torch.cuda.is_available() and not next(model.parameters()).is_cuda:
         model.cuda()
@@ -40,7 +42,7 @@ def valid_epoch(model, loader, loss_fun):
                 x = x.cuda() if x.device.type != 'cuda' else x
                 target = target.cuda() if target.device.type != 'cuda' else target
             out = model(x).squeeze(1)
-            loss = loss_fun(out.float(), target.float())
+            loss = loss_fn(out, target.float(), (~torch.isnan(x))) if masked_loss_fn else loss_fn(out, target.float())
             total_loss += loss.item()
             prediction = torch.round(out)
             correct += (prediction == target).sum().item()
@@ -77,8 +79,8 @@ def trash_in(model, trash_heap_size=500):
 
 
 def fold_cross_validate(model_fn, data_folds,
-                        optimizer_fn=torch.optim.SGD, loss_fn=nn.BCELoss(),
-                        epochs=125, learning_rate=0.01, batch_size=32, **optim_kwargs):
+                        epochs=125, learning_rate=0.01, batch_size=32,
+                        loss_fn=nn.BCELoss(), masked_loss_fn=False, optimizer_fn=torch.optim.SGD, **optim_kwargs):
     accuracy = []
     running_loss = []
     models = []
@@ -97,7 +99,7 @@ def fold_cross_validate(model_fn, data_folds,
         running_loss.append([])
         for epoch in range(epochs):
             model.train(True)
-            loss = train_epoch(model, train_loader, loss_fn, optimizer)
+            loss = train_epoch(model, train_loader, loss_fn, optimizer, masked_loss_fn=masked_loss_fn)
             running_loss[fold].append(loss)
         model.eval()
         correct = test_model(model, test_loader)
