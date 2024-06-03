3f29dfb38731a23b3ba012e25ee617aae5d914db
jiversivers <jdivers@uark.edu>
2024-06-03 10:16:25 -0500

    added warning for fetc model fail, but stil gets hook. Now jsut prints warning (for debugging)

--- my_modules/custom_models/classifier_models.py
+++ my_modules/custom_models/classifier_models.py
@@ -550,8 +550,9 @@ class FeatureExtractorToClassifier(nn.Module):
         try:
             _ = self.feature_extractor(x)
         except Exception as e:
-            if e == RuntimeError:
-                pass
+            print(f'{type(e).__name__}: {e}')
+            # if e == RuntimeError:
+
 
         # Clean up the hook
         fh.remove()
