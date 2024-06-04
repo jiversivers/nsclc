61e9bdd5b15193eb64b3e708c1d6d537c988b777
jiversivers <jdivers@uark.edu>
2024-06-04 15:10:06 -0500

    added no grad and removed acc * 100

--- my_modules/model_learning/model_evaluation.py
+++ my_modules/model_learning/model_evaluation.py
@@ -7,31 +7,32 @@ def calculate_auc_roc(model, loader, print_results=False, plot_results=False,
     model.eval()
     outs = torch.tensor([]).to(device)
     targets = torch.tensor([]).to(device)
-    for x, target in loader:
-        outs = torch.cat((outs, model(x)), dim=0)
-        targets = torch.cat((targets, target), dim=0)
-    thresholds, idx = torch.sort(outs.detach().squeeze())
-    sorted_targets = targets[idx]
-    tpr = []
-    fpr = []
-    acc = []
-    for t in thresholds:
-        positive_preds = thresholds >= t
-        true_positives = torch.logical_and(positive_preds, sorted_targets == 1)
-        true_negatives = torch.logical_and(~positive_preds, sorted_targets == 0)
-        false_positives = torch.logical_and(positive_preds, sorted_targets == 0)
-        false_negatives = torch.logical_and(~positive_preds, sorted_targets == 1)
-        tpr.append((torch.sum(true_positives) / (torch.sum(sorted_targets == 1))).item())
-        fpr.append((torch.sum(false_positives) / (torch.sum(sorted_targets == 0))).item())
-        acc.append((torch.sum(true_positives) + torch.sum(true_negatives)) / len(sorted_targets))
-    tpr.append(0.0)
-    fpr.append(0.0)
-    d_fpr = [f1 - f0 for f1, f0 in zip(fpr[:-1], fpr[1:])]
-    a = [t * df for df, t in zip(d_fpr, tpr[:-1])]
+    with torch.no_grad():
+        for x, target in loader:
+            outs = torch.cat((outs, model(x)), dim=0)
+            targets = torch.cat((targets, target), dim=0)
+        thresholds, idx = torch.sort(outs.detach().squeeze())
+        sorted_targets = targets[idx]
+        tpr = []
+        fpr = []
+        acc = []
+        for t in thresholds:
+            positive_preds = thresholds >= t
+            true_positives = torch.logical_and(positive_preds, sorted_targets == 1)
+            true_negatives = torch.logical_and(~positive_preds, sorted_targets == 0)
+            false_positives = torch.logical_and(positive_preds, sorted_targets == 0)
+            false_negatives = torch.logical_and(~positive_preds, sorted_targets == 1)
+            tpr.append((torch.sum(true_positives) / (torch.sum(sorted_targets == 1))).item())
+            fpr.append((torch.sum(false_positives) / (torch.sum(sorted_targets == 0))).item())
+            acc.append((torch.sum(true_positives) + torch.sum(true_negatives)) / len(sorted_targets))
+        tpr.append(0.0)
+        fpr.append(0.0)
+        d_fpr = [f1 - f0 for f1, f0 in zip(fpr[:-1], fpr[1:])]
+        a = [t * df for df, t in zip(d_fpr, tpr[:-1])]
 
     # Final metrics
     auc = sum(a)
-    best_acc = 100 * max(acc)
+    best_acc = max(acc)
     thresh = thresholds[acc.index(best_acc)]
 
     # Optional print of metrics
