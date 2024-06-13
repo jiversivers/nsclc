d671cc02f6ff2e5bb2e9c8a1935b3edee8ebd0dd
jiversivers <jdivers@uark.edu>
2024-06-13 12:39:03 -0500

    fixed squeeze, updated for pt_wise splitting, removed validation

--- jobs/xception_reg-mlp_w_atlases/fetc_mlp_w_atlases.py
+++ jobs/xception_reg-mlp_w_atlases/fetc_mlp_w_atlases.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 from my_modules.model_learning.loader_maker import split_augmented_data
 from my_modules.model_learning.model_metrics import score_model
-from my_modules.nsclc import NSCLCDataset
+from my_modules.nsclc import NSCLCDataset, patient_wise_train_test_splitter
 from my_modules.custom_models import MLPNet as MLP, FeatureExtractorToClassifier as FETC
 
 
@@ -33,13 +33,13 @@ def main():
         params.requires_grad = False
 
     # Set up the dataset for this model
-    # Images, no mask (feature extractor will hopefully handle this), normalized (already is),
+    # Images, no mask (feature extractor will hopefully handle this), normalized_to_max (already is),
     data = NSCLCDataset('data/NSCLC_Data_for_ML', ['orr'], device='cpu',
-                        label='Metastases', mask_on=False, use_atlas=True, chunk_atlas=True)
+                        label='Metastases', mask_on=False, use_atlas=True, use_patches=True)
     print('Normalizing data to channel max...')
     data.augment()
     data.transform_to_psuedo_rgb()
-    data.transforms = torch.squeeze()
+    data.transforms = torch.squeeze
     data.to(device)
 
     batch_size = 64
@@ -52,18 +52,13 @@ def main():
     classifier = MLP
 
     # Prepare data loaders
-    train_set, eval_set, test_set = split_augmented_data(data, augmentation_factor=5, split=(0.75, 0.15, 0.1))
+    train_set, test_set = patient_wise_train_test_splitter(data, n=3)
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=0,
                                                drop_last=(True if len(train_set) % batch_size == 1 else False))
-    eval_loader = torch.utils.data.DataLoader(eval_set, batch_size=batch_size, shuffle=False, num_workers=0,
-                                              drop_last=(True if len(eval_set) % batch_size == 1 else False))
-    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=0,
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True, num_workers=0,
                                               drop_last=(True if len(test_set) % batch_size == 1 else False))
 
     training_loss = []
-    evaluation_loss = []
-    evaluation_threshold = []
-    evaluation_accuracy = []
     testing_accuracy = []
     for lr in learning_rates:
         # Make full model
@@ -75,9 +70,6 @@ def main():
 
         # Nest iteration lists for tracking model learning
         training_loss.append([])
-        evaluation_threshold.append([])
-        evaluation_accuracy.append([])
-        evaluation_loss.append([])
         testing_accuracy.append([])
 
         # Find max number of epochs to consider and do that, checking along the way for others
@@ -101,24 +93,6 @@ def main():
                 epoch_loss += loss.item()
             training_loss[-1].append(epoch_loss / len(train_set))
 
-            # Validation
-            model.eval()
-            eval_loss = 0
-            with torch.no_grad():
-                for x, target in eval_loader:
-                    if torch.cuda.is_available() and not x.is_cuda:
-                        x = x.cuda()
-                    if torch.cuda.is_available() and not target.is_cuda:
-                        target = target.cuda()
-                    out = model(x)
-
-                    loss = loss_fn(out, target.unsqueeze(1))
-                    eval_loss += loss.item()
-                    scores = score_model(model, eval_loader)
-                evaluation_loss[-1].append(eval_loss / len(eval_set))
-                evaluation_threshold[-1].append(scores['Optimal Threshold from ROC'])
-                evaluation_accuracy[-1].append(scores['Accuracy at ROC Threshold'])
-
             # See if we are at one of our training length checkpoints. Save and test if we are
             if ep + 1 in epochs:
                 # save trained model at this many epochs
@@ -139,14 +113,7 @@ def main():
                     f.write('_____________________________________________________\n')
 
         plt.plot(range(1, 1 + epoch), training_loss[-1])
-        plt.plot(range(1, 1 + epoch), evaluation_loss[-1])
         plt.savefig(f'outputs/plots/loss_xception_to_mlp_w_atlas_{ep + 1}-Epochs_{lr}-LearningRate.png')
-        plt.close()
-        plt.plot(range(1, 1 + epoch), evaluation_accuracy[-1])
-        plt.savefig(f'outputs/plots/roc_thresh_acc_xception_to_mlp_w_atlas_{ep + 1}-Epochs_{lr}-LearningRate.png')
-        plt.close()
-        plt.plot(range(1, 1 + epoch), evaluation_threshold[-1])
-        plt.savefig(f'outputs/plots/roc_thresh_xception_to_mlp_w_atlas_{ep + 1}-Epochs_{lr}-LearningRate.png')
         plt.close()
 
 
