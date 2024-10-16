corrected normalization call

jobs/multi_model_tests_with_fad-nadh-orr/multi_model_tests.py
@@ -30,7 +30,7 @@ def main():
     ################
     data = NSCLCDataset('NSCLC_Data_for_ML', ['fad', 'nadh', 'orr'], device=torch.device('cpu'),
                         label='Metastases', mask_on=True)
-    data.normalize_channels('preset')
+    data.normalize_channels = 'preset'
     data.transforms = tvt.Compose([tvt.RandomVerticalFlip(p=0.25),
                                    tvt.RandomHorizontalFlip(p=0.25),
                                    tvt.RandomRotation(degrees=(-180, 180))])