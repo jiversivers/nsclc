ea486d9492e9d63c0913f0cc67d1acb538c17726
jiversivers <jdivers@uark.edu>
2024-10-25 12:26:36 -0500

    cleaned up code. Trying to find source of inconsistency in testing when combining sets. nothing glaring. but running again with datasets split at init

--- jobs/multi_model_tests_with_fad-nadh-shg-orr/multi_model_tests.py
+++ jobs/multi_model_tests_with_fad-nadh-shg-orr/multi_model_tests.py
@@ -28,21 +28,28 @@ def main():
     ################
     # Prepare data #
     ################
-    data = NSCLCDataset('NSCLC_Data_for_ML', ['fad', 'nadh', 'shg', 'orr'], device=torch.device('cpu'),
-                        label='Metastases', mask_on=True)
-    data.augment()
-    data.normalize_method = 'preset'
-    data.transforms = tvt.Compose([tvt.RandomVerticalFlip(p=0.25),
-                                   tvt.RandomHorizontalFlip(p=0.25),
-                                   tvt.RandomRotation(degrees=(-180, 180))])
-    data.to(device)
+    # To independent but identical (except for transformations) datasets
+    train_data = NSCLCDataset('NSCLC_Data_for_ML', ['fad', 'nadh', 'shg', 'orr'], device=torch.device('cpu'),
+                              label='Metastases', mask_on=True)
+    train_data.augment()
+    train_data.normalize_method = 'preset'
+    train_data.to(device)
+    train_data.transforms = tvt.Compose([tvt.RandomVerticalFlip(p=0.25),
+                                         tvt.RandomHorizontalFlip(p=0.25),
+                                         tvt.RandomRotation(degrees=(-180, 180))])
+
+    eval_test_data = NSCLCDataset('NSCLC_Data_for_ML', ['fad', 'nadh', 'shg', 'orr'], device=torch.device('cpu'),
+                                  label='Metastases', mask_on=True)
+    eval_test_data.augment()
+    eval_test_data.normalize_method = 'preset'
+    eval_test_data.to(device)
 
     # Random split datasets
-    subsampler = torch.utils.data.sampler.SubsetRandomSampler(range(data.patient_count))
+    subsampler = torch.utils.data.sampler.SubsetRandomSampler(range(train_data.patient_count))
     idx = [i for i in subsampler]
 
     # Get the image indices for all patients as nested lists
-    patient_subsets = [data.get_patient_subset(i) for i in idx]
+    patient_subsets = [train_data.get_patient_subset(i) for i in idx]
 
     # Find and remove any patients with no image indices
     idx_for_removal = []
@@ -53,10 +60,10 @@ def main():
         idx.remove(ix)
 
     # Get labels for all remaining patients
-    labels = [data.get_patient_label(i).item() for i in idx]
+    labels = [train_data.get_patient_label(i).item() for i in idx]
     image_counts = [0, 0]
     for i, label in zip(idx, labels):
-        image_counts[int(label)] += len(data.get_patient_subset(i))
+        image_counts[int(label)] += len(train_data.get_patient_subset(i))
 
     # Separate 0 and 1 labels (still shuffled)
     shuffled_zeros = [i for i, l in zip(idx, labels) if l == 0]
@@ -66,56 +73,71 @@ def main():
 
     # Split train, eval, and test sets
     train_pts = shuffled_zeros[3:-1] + shuffled_ones[3:-1]
-    train_idx = [data.get_patient_subset(i) for i in train_pts]
+    train_idx = [train_data.get_patient_subset(i) for i in train_pts]
     train_idx = [im for i in train_idx for im in i]
     random.shuffle(train_idx)
-    image_counts = [0, 0]
+    train_image_counts = [0, 0]
     for idx in train_pts:
-        label = data.get_patient_label(idx)
-        image_counts[int(label)] += len(data.get_patient_subset(idx))
-    print(f'Training set\n'
-          f'____________\n'
-          f'Non-metastatic: {len(shuffled_ones[3:-1])} with {image_counts[1]} images.\n'
-          f'Metastatic: {len(shuffled_zeros[3:-1])} with {image_counts[0]} images.\n'
-          f'Total: {len(train_pts)} Patients with {len(train_idx)} images.\n')
+        label = train_data.get_patient_label(idx)
+        train_image_counts[int(label)] += len(train_data.get_patient_subset(idx))
 
     eval_pts = shuffled_zeros[0:3] + shuffled_ones[0:3]
-    eval_idx = [data.get_patient_subset(i) for i in eval_pts]
+    eval_idx = [eval_test_data.get_patient_subset(i) for i in eval_pts]
     eval_idx = [im for i in eval_idx for im in i]
     random.shuffle(eval_idx)
-    image_counts = [0, 0]
+    eval_image_counts = [0, 0]
     for idx in eval_pts:
-        label = data.get_patient_label(idx)
-        image_counts[int(label)] += len(data.get_patient_subset(idx))
+        label = eval_test_data.get_patient_label(idx)
+        eval_image_counts[int(label)] += len(eval_test_data.get_patient_subset(idx))
+
+    test_pts = [shuffled_zeros[-1], shuffled_ones[-1]]
+    test_idx = [eval_test_data.get_patient_subset(i) for i in test_pts]
+    test_idx = [im for i in test_idx for im in i]
+    random.shuffle(test_idx)
+    test_image_counts = [0, 0]
+    for idx in test_pts:
+        label = eval_test_data.get_patient_label(idx)
+        test_image_counts[int(label)] += len(eval_test_data.get_patient_subset(idx))
+
+    comb_pts = eval_pts + test_pts
+    comb_idx = [eval_test_data.get_patient_subset(i) for i in comb_pts]
+    comb_idx = [im for i in comb_idx for im in i]
+    random.shuffle(comb_idx)
+    comb_image_counts = [0, 0]
+    for idx in comb_pts:
+        label = eval_test_data.get_patient_label(idx)
+        comb_image_counts[int(label)] += len(eval_test_data.get_patient_subset(idx))
+
+    print(f'Training set\n'
+          f'____________\n'
+          f'Non-metastatic: {len(shuffled_ones[3:-1])} with {train_image_counts[1]} images.\n'
+          f'Metastatic: {len(shuffled_zeros[3:-1])} with {train_image_counts[0]} images.\n'
+          f'Total: {len(train_pts)} Patients with {len(train_idx)} images.\n')
 
     print(f'Evaluation set\n'
           f'______________\n'
-          f'Non-metastatic: {len(shuffled_ones[0:3])} with {image_counts[1]} images.\n'
-          f'Metastatic: {len(shuffled_zeros[0:3])} with {image_counts[0]} images.\n'
+          f'Non-metastatic: {len(shuffled_ones[0:3])} with {eval_image_counts[1]} images.\n'
+          f'Metastatic: {len(shuffled_zeros[0:3])} with {eval_image_counts[0]} images.\n'
           f'Total: {len(eval_pts)} Patients with {len(eval_idx)} images.\n')
-
-    test_pts = [shuffled_zeros[-1], shuffled_ones[-1]]
-    test_idx = [data.get_patient_subset(i) for i in test_pts]
-    test_idx = [im for i in test_idx for im in i]
-    random.shuffle(test_idx)
-    image_counts = [0, 0]
-    for idx in test_pts:
-        label = data.get_patient_label(idx)
-        image_counts[int(label)] += len(data.get_patient_subset(idx))
 
     print(f'Testing set\n'
           f'____________\n'
-          f'Non-metastatic: {1} with {image_counts[1]} images.\n'
-          f'Metastatic: {1} with {image_counts[0]} images.\n'
+          f'Non-metastatic: {1} with {test_image_counts[1]} images.\n'
+          f'Metastatic: {1} with {test_image_counts[0]} images.\n'
           f'Total: {len(test_pts)} Patients with {len(test_idx)} images.\n')
 
-    print(f'Training patients: {train_pts}.\nEvaluation patients: {eval_pts}.\nTest patients: {test_pts}.\n')
+    print(f'Training patients: {train_pts}.\nEvaluation patients: {eval_pts}.\nTest patients: {test_pts}.\n'
+          f'CHECK -- Combined Eval-Test patients: {comb_pts}. '
+          f'Non-metastatic: {comb_image_counts[1]}. Metastatic: {comb_image_counts[0]}\n')
 
     # Create dataloaders for fold
     batch_size = 64
-    train_set = torch.utils.data.Subset(data, train_idx)
-    eval_set = torch.utils.data.Subset(data, eval_idx)
-    test_set = torch.utils.data.Subset(data, test_idx)
+    train_set = torch.utils.data.Subset(train_data, train_idx)
+    eval_set = torch.utils.data.Subset(eval_test_data, eval_idx)
+    test_set = torch.utils.data.Subset(eval_test_data, test_idx)
+    comb_set = torch.utils.data.Subset(eval_test_data, comb_idx)
+
+    # Create loaders
     train_loader = torch.utils.data.DataLoader(train_set,
                                                batch_size=batch_size, shuffle=True, num_workers=0,
                                                drop_last=(True if len(train_idx) % batch_size == 1 else False))
@@ -125,32 +147,35 @@ def main():
     test_loader = torch.utils.data.DataLoader(test_set,
                                               batch_size=batch_size, shuffle=False, num_workers=0,
                                               drop_last=(True if len(test_idx) % batch_size == 1 else False))
+    comb_loader = torch.utils.data.DataLoader(comb_set,
+                                              batch_size=batch_size, shuffle=False, num_workers=0,
+                                              drop_last=(True if len(comb_idx) % batch_size == 1 else False))
 
     #####################
     # Prepare model zoo #
     #####################
     # ResNet18
-    models = [ResNet18NPlaned(data.shape, start_width=64, n_classes=1)]
+    models = [ResNet18NPlaned(train_data.shape, start_width=64, n_classes=1)]
 
     # InceptionResNetV2 Feature Extractor (BigCoMET)
-    feature_extractor = AdaptedInputInceptionResNetV2(data.shape, num_classes=1000, pretrained=False)
+    feature_extractor = AdaptedInputInceptionResNetV2(train_data.shape, num_classes=1000, pretrained=False)
     classifier = CometClassifierWithBinaryOutput
-    models.append(FeatureExtractorToClassifier(data.shape,
+    models.append(FeatureExtractorToClassifier(train_data.shape,
                                                feature_extractor=feature_extractor,
                                                classifier=classifier, layer='inceptionresnetv2.conv2d_7b'))
 
     # Xception Feature Extractor
-    feature_extractor = AdaptedInputXception(data.shape, num_classes=1000, pretrained=False)
+    feature_extractor = AdaptedInputXception(train_data.shape, num_classes=1000, pretrained=False)
     classifier = torch.nn.Sequential(torch.nn.Linear(2048, 1), torch.nn.Sigmoid())
-    models.append(FeatureExtractorToClassifier(data.shape,
+    models.append(FeatureExtractorToClassifier(train_data.shape,
                                                feature_extractor=feature_extractor,
                                                classifier=classifier, layer='xception.conv4'))
 
     # Basic CNNs
-    models[len(models):] = [CNNet(data.shape),
-                            RegularizedCNNet(data.shape),
-                            ParallelCNNet(data.shape),
-                            RegularizedParallelCNNet(data.shape)]
+    models[len(models):] = [CNNet(train_data.shape),
+                            RegularizedCNNet(train_data.shape),
+                            ParallelCNNet(train_data.shape),
+                            RegularizedParallelCNNet(train_data.shape)]
 
     # Put all models on GPU if available
     for model in models:
@@ -196,18 +221,20 @@ def main():
         for model in models:
             model.train()
         for x, target in train_loader:
+            x = x.to(device)
+            target = target.to(device)
             for i, model in enumerate(models):
                 out = model(x)
                 outs[i] = torch.cat((outs[i], out.cpu().detach()), dim=0)
                 targets[i] = torch.cat((targets[i], target.cpu().detach()), dim=0)
                 loss = loss_function(out, target.unsqueeze(1))
+                optimizers[i].zero_grad()
                 loss.backward()
                 epoch_loss[i] += loss.item()
                 optimizers[i].step()
         for el, tl, ta, tx, ot, model in zip(epoch_loss, train_loss, train_auc, targets, outs, models):
             tl.append(el / len(train_set))
             ta.append(roc_auc_score(tx, ot))
-            print(f'>>> {model.name}: Train - Loss: {tl[-1]}. AUC: {ta[-1]}.')
 
         # Evaluation
         for i, (model, el, ea, tl) in enumerate(zip(models, eval_loss, eval_auc, train_loss)):
@@ -215,12 +242,13 @@ def main():
                                  threshold_type='roc')
             el.append(scores['Loss'])
             ea.append(scores['ROC-AUC'])
-            print(f' --> Eval - Loss: {el[-1]}. AUC: {ea[-1]}.')
+            print(f'>>> {model.name}: Train - Loss: {tl[-1]}. AUC: {ta[-1]}.')
+            print(f' --> Eval - Loss: {el[-1]:.4f}. AUC: {ea[-1]:.4f}.')
 
             with open(f'outputs/{model.name}/results.txt', 'a') as f:
                 f.write(f'\nEpoch {ep + 1}'
-                        f'>>> {model.name}: Train - Loss: {tl[-1]}. AUC: {ta[-1]}.'
-                        f'--> Eval - Loss: {el[-1]}. AUC: {ea[-1]}.')
+                        f'>>> {model.name}: Train - Loss: {tl[-1]:.4f}. AUC: {ta[-1]:.4f}.'
+                        f'--> Eval - Loss: {el[-1]:.4f}. AUC: {ea[-1]:.4f}.')
 
             if scores['ROC-AUC'] > best_score[i]:
                 best_score[i] = scores['ROC-AUC']
@@ -242,11 +270,6 @@ def main():
                 f'Final Train AUC - {ta[-1]:.4f}. Final Eval AUC - {ea[-1]:.4f}\n')
 
     # Testing
-    # Combination dataset of testing and eval sets
-    comb_set = torch.utils.data.ConcatDataset([eval_set, test_set])
-    comb_loader = torch.utils.data.DataLoader(comb_set,
-                                              batch_size=batch_size, shuffle=False, num_workers=0,
-                                              drop_last=(True if len(comb_set) % batch_size == 1 else False))
     for model in models:
         # full training model
         # on test set
@@ -302,17 +325,18 @@ def main():
     # Plot epoch-wise outputs
     for (model, tl, el, ta, ea) in zip(models, train_loss, eval_loss, train_auc, eval_auc):
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 5))
-        plt.supxlabel('Epochs')
         plt.suptitle(model.name)
 
         ax1.plot(range(1, epochs + 1), tl, label=f'{model.name} Training')
         ax1.plot(range(1, epochs + 1), el, label=f'{model.name} Evaluation')
+        ax1.set_xlabel('Epochs')
         ax1.set_ylabel('Loss')
         ax1.set_title('Training and Evaluation Losses')
         ax1.legend()
 
         ax2.plot(range(1, epochs + 1), ta, label=f'{model.name} Training')
         ax2.plot(range(1, epochs + 1), ea, label=f'{model.name} Evaluation')
+        ax1.set_ylabel('Epochs')
         ax2.set_ylabel('AUC')
         ax2.set_title('Training and Evaluation ROC-AUC')
         ax2.legend()
